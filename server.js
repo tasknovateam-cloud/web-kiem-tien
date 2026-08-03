80df5de7bcb20a452a17d07af838cbb1a12e2578
tasknovateam-cloud <email-cua-ban@gmail.com>
2026-08-03 11:13:35 +0700

    Fix PORT env for Render

--- server.js
+++ server.js
@@ -175,5 +175,9 @@ app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server đang chạy tại port ${PORT}`));
+// Mới: Sử dụng cổng của Render cấp, nếu chạy ở máy cục bộ (local) mới dùng 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server đang chạy tại port ${PORT}`);
+});
